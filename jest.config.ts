fb1702a08a70f1f0a1752e872abc181844b07e9f
mbxtech <mike.broers@yahoo.de>
2026-02-15 10:03:59 +0100

    chore(Tests): Ignoring e2e folder

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,7 @@
 module.exports = {
     preset: 'jest-preset-angular',
     setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-    testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+    testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/', '<rootDir>/e2e/'],
     moduleFileExtensions: ['ts', 'html', 'js', 'json'],
     transform: {
         '^.+\\.ts$': 'ts-jest', // Only transform .ts files
